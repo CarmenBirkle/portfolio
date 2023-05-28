3d4fddddb304aaeb250887529c1737d33a0549e3
Carmen Birkle <carmen.birkle@iu-study.org>
2023-05-28 16:36:59 +0200

    no scroll function if mobile menu open

--- src/app/mobile-nav/mobile-nav.component.ts
+++ src/app/mobile-nav/mobile-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { PagesBehaviorService } from '../pages-behavior.service';
 
 @Component({
@@ -7,11 +7,18 @@ import { PagesBehaviorService } from '../pages-behavior.service';
   styleUrls: ['./mobile-nav.component.scss'],
 })
 export class MobileNavComponent {
-  constructor(public pagesBehaviorService: PagesBehaviorService) {}
+  constructor(
+    public pagesBehaviorService: PagesBehaviorService,
+    private renderer: Renderer2
+  ) {}
 
   isMenuOpen = false;
 
   toggleMenu() {
-      this.isMenuOpen = !this.isMenuOpen;
+    this.isMenuOpen = !this.isMenuOpen;
+    this.isMenuOpen ? this.renderer.addClass(document.body, 'no-scroll') 
+                    : this.renderer.removeClass(document.body, 'no-scroll');
   }
+
+  
 }
