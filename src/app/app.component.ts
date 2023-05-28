7c4c6ba6d27a751d61add76c94b457ad7ee076ac
Carmen Birkle <carmen.birkle@iu-study.org>
2023-05-28 10:15:38 +0200

    translate function header nav and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'portfolio';
+
   constructor(public translate: TranslateService) {
     // translate.setDefaultLang('en');
     // translate.use('en');
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { PagesBehaviorService } from '../pages-behavior.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  constructor(public pagesBehaviorService: PagesBehaviorService) {}
 
-  
+export class HeaderComponent {
+  german = false;
+  language = 'EN';
+  flagPicture = 'assets/img/icons/german.png';
+
+  constructor(public pagesBehaviorService: PagesBehaviorService, public translate: TranslateService) {
+    translate.addLangs(['en', 'de']);
+    translate.setDefaultLang('en');
+  }
+
+toggleLanguage() {
+  // if (this.german) {
+  //   this.translate.use('en');
+  //   this.german = false;
+  //   this.flagPicture = 'assets/images/germany.png';
+  // } else {
+  //   this.translate.use('de');
+  //   this.german = true;
+  //   this.flagPicture = 'assets/images/usa.png';
+  // }
+
+  this.german = !this.german;
+  this.translate.use(this.german ? 'de' : 'en');
+  this.flagPicture = this.german ? 'assets/img/icons/english.png' : 'assets/img/icons/german.png';
+  this.language = this.german ? 'EN' : 'DE';
+}
 
 }
