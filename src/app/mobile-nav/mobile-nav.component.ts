ff131a2c3b8956a3ac24710bb531d4c080411146
Carmen Birkle <carmen.birkle@iu-study.org>
2023-05-28 12:22:40 +0200

    burger button first steps

--- src/app/mobile-nav/mobile-nav.component.ts
+++ src/app/mobile-nav/mobile-nav.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { PagesBehaviorService } from '../pages-behavior.service';
 
 @Component({
   selector: 'app-mobile-nav',
   templateUrl: './mobile-nav.component.html',
-  styleUrls: ['./mobile-nav.component.scss']
+  styleUrls: ['./mobile-nav.component.scss'],
 })
 export class MobileNavComponent {
+  constructor(public pagesBehaviorService: PagesBehaviorService) {}
 
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
--- src/app/pages-behavior.service.ts
+++ src/app/pages-behavior.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PagesBehaviorService {
-  constructor() {}
+  german = false;
+  language = 'EN';
+  flagPicture = 'assets/img/icons/german.png';
+
+  constructor(private translate: TranslateService) {
+    translate.addLangs(['en', 'de']);
+    translate.setDefaultLang('en');
+  }
+
+/**
+ * Toggle language between english and german
+ * change flag picture and the alt text
+ */ 
+  toggleLanguage() {
+    this.german = !this.german;
+    this.translate.use(this.german ? 'de' : 'en');
+    this.flagPicture = this.german
+      ? 'assets/img/icons/english.png'
+      : 'assets/img/icons/german.png';
+    this.language = this.german ? 'EN' : 'DE';
+  }
+
   /**
    * Scroll to top, will be used on logo and button up
    */
