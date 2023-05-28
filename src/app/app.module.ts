d3ab11b387da685dc69a1a3d5d11a40e0d2aa801
Carmen Birkle <carmen.birkle@iu-study.org>
2023-05-28 07:20:52 +0200

    main project structure  and fonts 700 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { ImprintComponent } from './imprint/imprint.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { MainComponent } from './main/main.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { SkillsComponent } from './skills/skills.component';
+import { MobileNavComponent } from './mobile-nav/mobile-nav.component';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
     return new TranslateHttpLoader(http);
 }
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ImprintComponent, HeaderComponent, FooterComponent, PresentationComponent, MainComponent, AboutMeComponent, PortfolioComponent, ContactComponent, SkillsComponent, MobileNavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
