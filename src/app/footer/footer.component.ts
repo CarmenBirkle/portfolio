1769f74cbfe89b33a8f80bca6fca501416b8cceb
Carmen Birkle <carmen.birkle@iu-study.org>
2023-06-09 19:54:18 +0200

    footer section

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { PagesBehaviorService } from '../pages-behavior.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+  constructor(public pagesBehaviorService: PagesBehaviorService) {}
 }
