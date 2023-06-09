642d9028700ff6b19feabba193690603d69e0777
Carmen Birkle <carmen.birkle@iu-study.org>
2023-06-09 08:16:08 +0200

    console log form element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +20,7 @@ import { MobileNavComponent } from './mobile-nav/mobile-nav.component';
 import { ProjectComponent } from './project/project.component';
 
 
+
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
     return new TranslateHttpLoader(http);
@@ -29,6 +30,7 @@ export function HttpLoaderFactory(http: HttpClient) {
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
     TranslateModule.forRoot({
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
-import { Component, NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, ElementRef, NgModule, ViewChild } from '@angular/core';
+
+import { FormsModule, NgForm } from '@angular/forms'; //
 import { PagesBehaviorService } from '../pages-behavior.service';
 
 @Component({
@@ -9,6 +10,8 @@ import { PagesBehaviorService } from '../pages-behavior.service';
 })
 export class ContactComponent {
   constructor(public pagesBehaviorService: PagesBehaviorService) {}
+
+  @ViewChild('myForm') myForm!: NgForm;
 
   name = { value: '', isFocused: false };
   email = { value: '', isFocused: false };
@@ -23,5 +26,10 @@ export class ContactComponent {
   onBlur(field: any) {
     field.isFocused = false;
   }
-}
 
+  sendMail() {
+   
+    console.log('send mail');
+    console.log(this.name.value, this.email.value, this.message.value);
+  }
+}
