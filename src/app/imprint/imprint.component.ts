5e8e9f25f59a7a97b0a443289e7f3ec8cbf15f91
Carmen Birkle <carmen.birkle@iu-study.org>
2023-06-10 07:32:52 +0200

    ajust css

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { PagesBehaviorService } from '../pages-behavior.service';
 
 @Component({
   selector: 'app-imprint',
   templateUrl: './imprint.component.html',
-  styleUrls: ['./imprint.component.scss']
+  styleUrls: ['./imprint.component.scss'],
 })
 export class ImprintComponent {
-
+  constructor(public pagesBehaviorService: PagesBehaviorService) {}
 }
